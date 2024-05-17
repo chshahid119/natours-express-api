5ff81f3aa122eda1fcb4e990615032839dacd170
chshahid119 <shahidchaudhary0729@gmail.com>
2024-05-17 22:09:02 +0500

    Adding a Nested GET Endpoint

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,6 +6,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router({ mergeParams: true });
 
 // POST /tour/234fad4/reviews
+// POST /tour/234fad4/reviews
 // POST /reviews
 
 router
