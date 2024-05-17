4a506424b93b0fa24f41c7c5ac00f9e08bde2ba0
chshahid119 <shahidchaudhary0729@gmail.com>
2024-05-18 03:29:46 +0500

    Adding Missing Authentication & Authorization (also in POSTMAN)

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,18 +2,15 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-// mergeParams use krty hain ta ky child apnay parent ki :tourId ko access kar saky jo ky by default mergeParams: false hoti hai
+/*mergeParams use krty hain ta ky child apnay parent ki :tourId ko access kar saky jo
+ ky by default mergeParams: false hoti hai*/
 const router = express.Router({ mergeParams: true });
-
-// POST /tour/234fad4/reviews
-// GET /tour/234fad4/reviews
-// POST /reviews
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -22,7 +19,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
