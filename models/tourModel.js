a39a2d950bc4f103638dfcfd1487a701669cb4ef
chshahid119 <shahidchaudhary0729@gmail.com>
2024-05-20 10:43:06 +0500

    Preventing Multiple Reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -34,8 +34,9 @@ const tourSchema = new mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: [1, 'Must be at least 1, got {VALUE}'],
-      max: 5
+      min: [1, 'Must be above 1.0'],
+      max: [5, 'Rating must be below 1.0'],
+      set: val => Math.round(val * 10) / 10 //4.66666,46.6666,47,4.7
     },
     ratingsQuantity: {
       type: Number,
