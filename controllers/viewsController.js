0de000776ad309a28a544a6b892b675026cb28f3
chshahid119 <shahidchaudhary0729@gmail.com>
2024-05-21 20:18:28 +0500

    Building the Tour Overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,6 +5,13 @@ exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
+  tours.forEach(tour => {
+    tour.formattedDate = new Date(tour.startDates[0]).toLocaleString('en-us', {
+      month: 'long',
+      year: 'numeric'
+    });
+  });
+
   // 2) Build template
 
   // 3) Render that template using tour data from 1
