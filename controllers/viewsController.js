d9be959e100344ccd9c2410862fd96ea8e47174f
chshahid119 <shahidchaudhary0729@gmail.com>
2024-05-24 03:55:00 +0500

    Building the Login & Sign Up Screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
@@ -20,7 +20,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -34,3 +34,13 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
+
+exports.getSignUpForm = (req, res, next) => {
+  res.status(200).render('signup');
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,15 +3,9 @@ const viewsController = require('../controllers/viewsController');
 
 const router = express.Router();
 
-// We created that route just for test
-// router.get('/', (req, res) => {
-//   res.status(200).render('base', {
-//     tour: 'The Forest Hiker',
-//     user: 'Jonas'
-//   });
-// });
-
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
+router.get('/login', viewsController.getLoginForm);
+router.get('/signup', viewsController.getSignUpForm);
 
 module.exports = router;
